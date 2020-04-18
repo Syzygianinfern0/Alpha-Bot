e5ff9dc0c83428a11b82c32930b99e4b9128c582
Syzygianinfern0 <spsharan2000@gmail.com>
2020-04-18 16:25:19 +0530

    Timeout for Dead Link! Download status looks better

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -85,16 +85,16 @@ def get_readable_message():
     with download_dict_lock:
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<i>{download.name()}</i> - "
-            msg += download.status()
+            msg += f"<b>Name:</b> <code>{download.name()}</code>"
+            msg += f"\n<b>Status:</b> <code>{download.status()}</code>"
             if download.status() != MirrorStatus.STATUS_ARCHIVING:
-                msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code> of " \
-                    f"{download.size()}" \
-                    f" at {download.speed()}, ETA: {download.eta()} "
+                msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code>" \
+                    f"\n<b>Size:</b> {download.size()}" \
+                    f"\n<b>Speed:</b> {download.speed()}, \n<b>ETA:</b> {download.eta()} "
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                 if hasattr(download, 'is_torrent'):
-                    msg += f"| P: {download.download().connections} " \
-                           f"| S: {download.download().num_seeders}"
+                    msg += f"\n<b>Peers</b>: {download.download().connections} " \
+                           f"<b>Seeders</b>: {download.download().num_seeders}"
             msg += "\n\n"
         return msg
 
